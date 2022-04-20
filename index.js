d1e9c61a9fcee33abc8a1fe88d070be2560ffc3e
Eduardo Nascimento <edwardo.xavier@gmail.com>
2022-04-19 23:01:01 -0300

    Update of code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require ('express');//Importa o express para a API!
 const cors = require ('cors');//Importa o cors para a API!
-const router = require ('./src/routes/paletasRoutes');
+const router = require ('./src/routes/paletasRoutes');//Importando arquivo da pasta route!!!----->
 
 const app = express(); //Dita ao app, que use o express como framework!
 const port = 3002;//Define a porta padrão! 
--- src/routes/paletasRoutes.js
+++ src/routes/paletasRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = exprex.Router();
+const router = express.Router();
 
 const paletasControllers = require('../controllers/paletasControllers');
 
